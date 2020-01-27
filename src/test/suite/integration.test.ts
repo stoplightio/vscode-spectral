3973b8aa8dc531dab5a6b4f7d4f932ccb9dfe2f0
Mike Ralphson <mike.ralphson@gmail.com>
2020-01-27 16:07:20 +0000

    test: add integration.test.ts

---
+++ src/test/suite/integration.test.ts
@@ -0,0 +1,20 @@
+import * as assert from 'assert';
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from 'vscode';
+// import * as myExtension from '../extension';
+
+suite('Integration Test Suite', () => {
+	vscode.window.showInformationMessage('Start all integration tests.');
+
+  test("Should start extension @integration", () => {
+    const ext = vscode.extensions.getExtension(
+        "stoplight.spectral"
+    );
+    assert.ok(ext);
+    const started = ext!.isActive;
+    assert.equal(started, true);
+  });
+
+});
