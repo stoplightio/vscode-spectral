b6f377a73e7b265d2e3d18f8ae091dbd0a5c503d
Mike Ralphson <mike.ralphson@gmail.com>
2020-02-06 22:32:51 +0000

    tests: more extension tests pt 1

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,9 +1,18 @@
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import { DiagnosticSeverity } from '@stoplight/types';
 import * as assert from 'assert';
+import * as fs from 'fs';
+import * as path from 'path';
 import * as vscode from 'vscode';
+import * as linter from '../../linter';
 import * as utils from '../../utils';
+
+import { IRunOpts, ISpectralFullResult } from '@stoplight/spectral';
+import { DiagnosticSeverity } from '@stoplight/types';
+
+const SLOW_TIMEOUT_MS = 5000;
+const TEST_BASE = '../../../src/test/fixtures';
+const linterProvider = new linter.LinterCache();
 
 suite('Extension Test Suite', () => {
   vscode.window.showInformationMessage('Start all tests.');
@@ -14,4 +23,26 @@ suite('Extension Test Suite', () => {
     assert.equal(vscode.DiagnosticSeverity.Information, utils.ourSeverity(DiagnosticSeverity.Information));
     assert.equal(vscode.DiagnosticSeverity.Warning, utils.ourSeverity(DiagnosticSeverity.Warning));
   });
+
+  test('Linting a plaintext document', () => {
+    return new Promise(async (resolve, reject) => {
+      const plaintextPath = path.resolve(__dirname, TEST_BASE, 'plaintext.txt');
+      const plaintextUri = vscode.Uri.parse(plaintextPath);
+      const plaintextContents = fs.readFileSync(plaintextPath, 'utf8');
+      try {
+        const spectral = await linterProvider.getLinter(plaintextUri);
+        const linterOptions: IRunOpts = {
+          resolve: { documentUri: plaintextUri.toString() },
+        };
+        const output = await spectral.runWithResolved(plaintextContents, linterOptions);
+        assert.ok(output.hasOwnProperty('resolved'), 'Check for output.resolved');
+        assert.ok(output.hasOwnProperty('results'), 'Check for output.results');
+        assert.equal(output.results.length, 1, 'Should be one result');
+        assert.equal(output.results[0].code, 'unrecognized-format', 'Check result code');
+        resolve(output);
+      } catch (ex) {
+        reject(ex);
+      }
+    });
+  }).timeout(SLOW_TIMEOUT_MS);
 });
