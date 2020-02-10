63ed7a0b40d32c1e40c404ead29c80cfe44508e6
Mike Ralphson <mike.ralphson@gmail.com>
2020-02-10 15:06:52 +0000

    tests: manually install extension dependencies

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,23 +1,34 @@
+import * as cp from 'child_process';
 import * as path from 'path';
 
-import { runTests } from 'vscode-test';
+import { downloadAndUnzipVSCode, resolveCliPathFromVSCodeExecutablePath, runTests } from 'vscode-test';
 
 async function main() {
-	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+  try {
+    // The folder containing the Extension Manifest package.json
+    // Passed to `--extensionDevelopmentPath`
+    const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
-		// The path to test runner
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './suite/index');
+    // The path to test runner
+    // Passed to --extensionTestsPath
+    const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
-		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
-	} catch (err) {
-		console.error('Failed to run tests');
-		process.exit(1);
-	}
+    const vscodeExecutablePath = await downloadAndUnzipVSCode('1.40.1');
+    const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
+
+    // Use cp.spawn / cp.exec for custom setup
+    cp.spawnSync(cliPath, ['--install-extension', 'redhat.vscode-yaml'], {
+      encoding: 'utf-8',
+      stdio: 'inherit',
+    });
+
+    // Download VS Code, unzip it and run the integration test
+    await runTests({ vscodeExecutablePath, extensionDevelopmentPath, extensionTestsPath });
+  } catch (err) {
+    console.error('Failed to run tests', err.message);
+    process.exit(1);
+  }
 }
 
+// tslint:disable-next-line: no-floating-promises
 main();
