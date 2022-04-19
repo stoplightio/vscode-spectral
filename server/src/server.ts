c072111ffb0f63add66d06b54bd58e9048723cdc
Jakub Rożek <jakub@stoplight.io>
2022-04-19 20:46:01 +0200

    chore: support cjs

--- server/src/server.ts
+++ server/src/server.ts
@@ -21,7 +21,7 @@ import { Ruleset } from '@stoplight/spectral-core';
 import { asyncapi, oas } from '@stoplight/spectral-rulesets';
 import { fetch } from '@stoplight/spectral-runtime';
 import { bundleAndLoadRuleset } from '@stoplight/spectral-ruleset-bundler/with-loader';
-import { builtins } from '@stoplight/spectral-ruleset-bundler/plugins/builtins';
+import { commonjs } from '@stoplight/spectral-ruleset-bundler/plugins/commonjs';
 import {
   ExtensionSettings,
   TextDocumentSettings,
@@ -215,7 +215,7 @@ function resolveSettings(document: TextDocument): Thenable<TextDocumentSettings>
 
         connection.sendNotification(StartWatcherNotification.type, { path: rulesetFile });
         try {
-          settings.ruleset = await bundleAndLoadRuleset(rulesetFile, { fs, fetch }, [builtins()]);
+          settings.ruleset = await bundleAndLoadRuleset(rulesetFile, { fs, fetch }, [commonjs()]);
         } catch (err) {
           showErrorMessage(docPath, `Unable to read ruleset at ${rulesetFile}. ${err}`);
         }
